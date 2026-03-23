d9a88f6c41cf927b64e47be2062dc80b5c4725e6
Kohinoor <Knoorchauhan@gmail.com>
2026-03-23 09:11:47 -0600

    Enhance Flask app configuration and route registration
    Updated app.py to match the whole backend

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,41 @@
 #Main entry point, creates flask app
-from flask import Flask
-from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+from flask import Flask
+from flask_cors import CORS
+
 
 load_dotenv() #loads env vars
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev_secret_key")
+app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
+app.config["SESSION_COOKIE_HTTPONLY"] = True
+
 CORS(app)
 
 port = int(os.getenv("PORT", 5000))
 
-# import and register routes here
+# import and register routes here -> if we dont register app.py will never know tht those routes exist
 from routes.analysis_routes import analysis_bp #imports route file
 app.register_blueprint(analysis_bp) #registers route file with flask
 
 from routes.auth_routes import auth_bp
 app.register_blueprint(auth_bp, url_prefix="/auth") #in frontend we write auth/sigup etc
+
+from routes.scenario_routes import scenario_bp
+app.register_blueprint(scenario_bp, url_prefix="/scenario")
+
+# from routes.preference_routes import preference_bp
+# app.register_blueprint(preference_bp, url_prefix="/preference")
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True, port=port)
